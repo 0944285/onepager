10e2285f262be641e83c60bfda01473917299e77
0944285 <0944285@hr.nl>
2021-03-27 00:35:56 +0100

    header done

--- wp-content/themes/skelementor/header.php
+++ wp-content/themes/skelementor/header.php
@@ -4,6 +4,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="wp-content/themes/skelementor/style.css" type="text/css" media="screen" />
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
